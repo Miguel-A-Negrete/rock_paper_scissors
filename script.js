69a56fe1d79e9aef74fbefe764ec5d842f8aaa47
Miguel-A-Negrete <154093586+Miguel-A-Negrete@users.noreply.github.com>
2025-05-04 16:12:50 -0500

    added main functions for Rock, paper and Scissors added conditionals for the win

---
+++ script.js
@@ -0,0 +1,43 @@
+let humanScore = 0;
+let computerScore = 0;
+
+function getComputerChoice(){
+    random_number = Math.floor(Math.random() * 3)+1;
+    switch(random_number){
+        case 1:
+            return ("Rock");
+        case 2:
+            return ("Paper");
+        case 3:
+            return ("Scissors");
+        default:
+            console.log("default");
+    }
+    
+}
+
+function getHumanChoice(){
+   let answer = prompt("Rock, Paper or Scissors");
+   answer = answer.charAt(0).toUpperCase() + answer.slice(1).toLowerCase();
+   return answer;
+}
+
+
+
+function playRound(humanChoice, computerChoice){
+    if(humanChoice=="Rock" && computerChoice=="Scissors") {
+        return("You won! Rock beats Scissors")
+    }
+    else if(humanChoice=="Paper" && computerChoice=="Rock"){
+        return("You won! Paper beats Rock")
+    }
+    else if(humanChoice=="Scissors" && computerChoice=="Paper"){
+        return("You won! Scissors beats Paper")
+    }
+
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
